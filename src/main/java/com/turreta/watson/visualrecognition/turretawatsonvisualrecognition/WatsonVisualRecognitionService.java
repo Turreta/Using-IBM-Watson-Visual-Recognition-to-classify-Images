dcb91c6e6e0c85f33b8270f37ff3f0dd933682f2
Turreta.com <Turreta@users.noreply.github.com>
2017-11-06 01:07:15 +0800

    Update WatsonVisualRecognitionService.java
    Changed API key but it had been deleted on the server.

--- src/main/java/com/turreta/watson/visualrecognition/turretawatsonvisualrecognition/WatsonVisualRecognitionService.java
+++ src/main/java/com/turreta/watson/visualrecognition/turretawatsonvisualrecognition/WatsonVisualRecognitionService.java
@@ -20,7 +20,7 @@ public class WatsonVisualRecognitionService
 			// Default endpoint is https://gateway-a.watsonplatform.net/visual-recognition/api
 			visualRecognition = new VisualRecognition(
 					VisualRecognition.VERSION_DATE_2016_05_20,
-					"d948f9300c552d0cabf2213518fe8f60693a47c9");
+					"d948f9300c-deleted-already-on-server-e8f60693a47c9");
 		}
 	}
 
